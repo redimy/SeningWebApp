23b2db23df350d5e1d29a7c1d1cdd5da6c73828e
Dimitri Granados <redimy@outlook.com>
2020-04-26 17:05:07 -0500

    Carousel new images, carousel responsive - Navbar size and responsive, bug fix when selecting a new page, - favicon changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+// imports: [RouterModule.forRoot(routes, { useHash:true })],
+//para servidores como apache
