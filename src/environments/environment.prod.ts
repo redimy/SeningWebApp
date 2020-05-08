2fea06a7b58fcd071fcbb359f930ab83c04ccebe
Dimitri Granados <redimy@outlook.com>
2020-05-08 15:55:29 -0500

    Bug navbar fixed, instagram buttoms

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDu0GBJsN1eHjBtD4S4HF0scHvbRd6u2Y4",
+    authDomain: "sening-studio.firebaseapp.com",
+    databaseURL: "https://sening-studio.firebaseio.com",
+    projectId: "sening-studio",
+    storageBucket: "sening-studio.appspot.com",
+    messagingSenderId: "974855556293",
+    appId: "1:974855556293:web:f8443b2f1d44a407d4f4ac",
+    measurementId: "G-BFTV045W5M",
+  },
 };
